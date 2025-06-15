64f3ae6964069754eec133177a6c0e957a81b23f
Theekshana-Gimhan <theekshana3942@gmail.com>
2025-06-15 22:28:35 +0530

    Revert "Reapply "Reapply "target test"""
    This reverts commit e1569e5032f350d5f114cf3c3cfbb2ff53e84103.

--- Models/KPIAuditTrail.cs
+++ Models/KPIAuditTrail.cs
@@ -6,26 +6,34 @@ public class KPIAuditTrail
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     public string UserId { get; set; }
+
     [ForeignKey("UserId")]
     public ApplicationUser? User { get; set; }
+
     [Required]
     [StringLength(20)]
-    public string ActionType { get; set; }
+    public string ActionType { get; set; } = "Update";
+
     [Required]
     [StringLength(100)]
     public string Action { get; set; }
-    public DateTime Timestamp { get; set; }
+
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
     public string? OldValues { get; set; }
+
     public string? NewValues { get; set; }
+
     public int? TargetId { get; set; }
+
     [ForeignKey("TargetId")]
     public KPITarget? Target { get; set; }
+
     public bool IsSuccess { get; set; }
+
     [StringLength(500)]
     public string? ErrorMessage { get; set; }
-
-    [Timestamp]
-    public byte[]? RowVersion { get; set; }
 }
