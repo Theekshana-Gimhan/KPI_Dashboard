88956cb535788f7b65c677fe92b8523229365fb0
Theekshana-Gimhan <theekshana3942@gmail.com>
2025-06-15 22:28:43 +0530

    Reapply "Reapply "Reapply "target test"""
    This reverts commit 64f3ae6964069754eec133177a6c0e957a81b23f.

--- Models/AdmissionKPI.cs
+++ Models/AdmissionKPI.cs
@@ -18,6 +18,8 @@ namespace KPI_Dashboard.Models
         [Range(0, int.MaxValue, ErrorMessage = "Number of Consultations must be non-negative.")]
         public int Consultations { get; set; }
 
+        [Required]
+        [StringLength(450)]
         public string UserId { get; set; } = string.Empty;
     }
 }
